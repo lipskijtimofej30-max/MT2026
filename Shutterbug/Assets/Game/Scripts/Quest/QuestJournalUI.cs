661d12f451fc28196e744674ff7831de99c0251c
Tima <lipskijtimofej30@gmail.com>
2026-04-25 16:01:16 +0300

    new animation and occulusion culling

--- Shutterbug/Assets/Game/Scripts/Quest/QuestJournalUI.cs
+++ Shutterbug/Assets/Game/Scripts/Quest/QuestJournalUI.cs
@@ -22,7 +22,6 @@ namespace Game.Scripts.Quest
         [SerializeField] private CanvasGroup _canvasGroup;
         [SerializeField] private float _showDuration = 0.4f;
         [SerializeField] private float _hideDuration = 0.3f;
-        [SerializeField] private float _showMoveDistance = 100f; 
         [SerializeField] private Ease _showEase = Ease.OutBack;
         [SerializeField] private Ease _hideEase = Ease.InBack; 
 
@@ -30,7 +29,6 @@ namespace Game.Scripts.Quest
         private IAnimalInPhotoProvider _photoProvider;
         private PlayerController _playerController;
         
-        private Vector2 _originalAnchoredPosition;
         private Coroutine _currentAnimationCoroutine;
         private bool _isOpen;
 
@@ -45,7 +43,6 @@ namespace Game.Scripts.Quest
 
         private void Start()
         {
-            _originalAnchoredPosition = _windowRoot.anchoredPosition;
             Close();
         }
         
@@ -69,7 +66,6 @@ namespace Game.Scripts.Quest
 
             if (visible)
             {
-                _windowRoot.anchoredPosition = new Vector2(_originalAnchoredPosition.x, 0);
                 _windowRoot.localScale = Vector3.one;
                 _canvasGroup.alpha = 1f;
                 Open();
@@ -105,7 +101,6 @@ namespace Game.Scripts.Quest
             _isOpen = true;
 
             _canvasGroup.alpha = 0f;
-            _windowRoot.anchoredPosition = _originalAnchoredPosition - Vector2.up * _showMoveDistance;
             _windowRoot.localScale = Vector3.zero;
             _windowRoot.gameObject.SetActive(true);
 
@@ -140,12 +135,10 @@ namespace Game.Scripts.Quest
         {
             _isOpen = true;
 
-            _windowRoot.anchoredPosition = _originalAnchoredPosition - Vector2.up * _showMoveDistance;
             _windowRoot.localScale = Vector3.zero;
-            _canvasGroup.alpha = 1f;
+            _canvasGroup.alpha = 0f;
 
             var sequence = DOTween.Sequence();
-            sequence.Join(_windowRoot.DOAnchorPos(_originalAnchoredPosition, _showDuration).SetEase(_showEase));
             sequence.Join(_windowRoot.DOScale(1f, _showDuration).SetEase(_showEase));
             sequence.Join(_canvasGroup.DOFade(1f, _showDuration).SetEase(_showEase));
 
@@ -155,14 +148,11 @@ namespace Game.Scripts.Quest
         private IEnumerator HideAnimation()
         {
             var sequence = DOTween.Sequence();
-            sequence.Join(_windowRoot
-                .DOAnchorPos(_originalAnchoredPosition - Vector2.up * _showMoveDistance, _hideDuration)
-                .SetEase(_hideEase));
             sequence.Join(_windowRoot.DOScale(0f, _hideDuration).SetEase(_hideEase));
             sequence.Join(_canvasGroup.DOFade(0f, _hideDuration).SetEase(_hideEase));
 
             yield return sequence.WaitForCompletion();
-            
+
             _detailsPanel.gameObject.SetActive(false);
         }
     }
